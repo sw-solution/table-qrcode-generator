1fcc3c6ae2477895fa77030b2549afd63be08d38
bstar1227 <predragciric@yandex.com>
2021-09-21 16:04:08 +1000

    added bulk image download as a zip

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import CSVReader from "react-csv-reader";
 import Card from './components/card';
 import { useState } from 'react';
 
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { toPng } from 'html-to-image';
+import { saveAs } from 'file-saver';
+
 function App() {
   const [cards, setCards] = useState([]);
 
   const parseCSV = (data, fileInfo) => {
-    if( data && data.length ) {
+    if (data && data.length) {
       var arr = [];
 
-      data.map( (item, index) => item.place && item.id && item.name && arr.push( { uid: index, place: item.place, id: item.id, name: item.name } ) );
+      data.map((item, index) => item.place && item.id && item.name && arr.push({ uid: index, place: item.place, id: item.id, name: item.name }));
 
       setCards(arr);
     }
@@ -23,34 +31,60 @@ function App() {
     transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
   };
 
+  const downloadImages = () => {
+    const zip = require('jszip')();
+
+    cards.map((item, index) => {
+      var node = document.getElementById('card_item_download_' + item.uid)
+
+      return toPng(node.parentElement)
+        .then((dataUrl) => {
+
+          zip.file(item.name + ".png", dataUrl.split(',')[1], {base64: true});
+
+          if (index === cards.length - 1) {
+            zip.generateAsync({ type: "blob" }).then(content => {
+              saveAs(content, "table-qrcode.zip");
+            });
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    })
+  }
+
   return (
-      <div className="container">
-          <CSVReader
-              cssClass="react-csv-input"
-              onFileLoaded={parseCSV}
-              parserOptions={papaparseOptions}
-          />
+    <Container id="qrcode_app" className="text-center">
+      <CSVReader
+        cssClass="react-csv-input"
+        onFileLoaded={parseCSV}
+        parserOptions={papaparseOptions}
+      />
 
-          <div className="card-list">
-              { 
-                cards.map((item, index) => {
-                  return (
-                    <Card key={index} detail={item} />
-                  )
-                })
-              }
-          </div>
+      {cards.length > 0 && <Button className="my-3" onClick={() => downloadImages()}> Download All </Button>
+      }
 
-          <div className="download-card-list">
-            { 
-              cards.map((item, index) => {
-                return (
-                  <Card key={index} detail={item} downloadCard={true} />
-                )
-              })
-            }
-          </div>
+      <div className="card-list">
+        {
+          cards.map((item, index) => {
+            return (
+              <Card key={index} detail={item} />
+            )
+          })
+        }
       </div>
+
+      <div className="download-card-list">
+        {
+          cards.map((item, index) => {
+            return (
+              <Card key={index} detail={item} downloadCard={true} />
+            )
+          })
+        }
+      </div>
+    </Container>
   );
 }
 
